c8f4d72fb9956182953e4e5bdca5d5f4e63a65ee
Rishabhqleverlabs <32612872+Rishabhqleverlabs@users.noreply.github.com>
2018-04-19 23:57:12 +0530

    Update planquadrat.sql

--- admin/planquadrat.sql
+++ admin/planquadrat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2018 at 11:48 AM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: Apr 19, 2018 at 07:15 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -55,18 +55,19 @@ CREATE TABLE `project` (
   `headertext` longtext NOT NULL,
   `bodytext` longtext NOT NULL,
   `contractor` text,
-  `projecttype` text
+  `projecttype` text,
+  `relatedprojects` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `project`
 --
 
-INSERT INTO `project` (`id`, `blogtitle`, `dates`, `images`, `headertext`, `bodytext`, `contractor`, `projecttype`) VALUES
-(51, 'sadfjkl;ewruiop', '2016-11-30', ' 1523862411-csm_11010-titelbild-01_62a0f9f6f33510.jpg', '<p>sadfgjkl;euio</p>', 'resdfgjkl;fjkl', 'contractor', 'project type'),
-(53, 'jdfhkjsdhfkjsdhfjsdhj', '2012-08-27', ' 1523864898-csm_chronik-2011-03-thumbnail_7840155d768888.jpg', '<p>jhdkjhfkjsdhfskjdh</p>', 'kjhkjdhfkjsdhfkjsdh', 'contractor', 'project type'),
-(54, 'asdfghjkl', '2016-10-29', ' 1523867470-csm_chronik-2009-03_fb255c88767770.jpg 1523867470-csm_jasmin-winter-03_02c903f2da9d57.jpg 1523867470-csm_sabrina_stelz-03_c4d393fbdfff92.jpg 1523867470-csm_chronik-2009-03_fb255c88767770.jpg', '<p>wertyuio</p>', 'ertgyhjkl;', 'contractor', 'project type'),
-(55, 'asdfghjklertyui', '2016-11-30', ' 1523871467-csm_boys_day_2017_4a5354ae963485.jpg 1523871467-csm_chronik-2009-03_1b9186df247770.jpg 1523871467-csm_chronik-2011-03-thumbnail_7840155d768888.jpg', '<p>sedghjkl;dfghjkl</p>', 'fghjkl;dfgh', 'building contractor', 'project type');
+INSERT INTO `project` (`id`, `blogtitle`, `dates`, `images`, `headertext`, `bodytext`, `contractor`, `projecttype`, `relatedprojects`) VALUES
+(51, 'sadfjkl;ewruiop', '2016-11-30', ' 1523862411-csm_11010-titelbild-01_62a0f9f6f33510.jpg', '<p>sadfgjkl;euio</p>', 'resdfgjkl;fjkl', 'contractor', 'project type', ''),
+(53, 'jdfhkjsdhfkjsdhfjsdhj', '2012-08-27', ' 1523864898-csm_chronik-2011-03-thumbnail_7840155d768888.jpg', '<p>jhdkjhfkjsdhfskjdh</p>', 'kjhkjdhfkjsdhfkjsdh', 'contractor', 'project type', ''),
+(54, 'asdfghjkl', '2016-10-29', ' 1523867470-csm_chronik-2009-03_fb255c88767770.jpg 1523867470-csm_jasmin-winter-03_02c903f2da9d57.jpg 1523867470-csm_sabrina_stelz-03_c4d393fbdfff92.jpg 1523867470-csm_chronik-2009-03_fb255c88767770.jpg', '<p>wertyuio</p>', 'ertgyhjkl;', 'contractor', 'project type', ''),
+(55, 'asdfghjklertyui', '2016-11-30', ' 1523871467-csm_boys_day_2017_4a5354ae963485.jpg 1523871467-csm_chronik-2009-03_1b9186df247770.jpg 1523871467-csm_chronik-2011-03-thumbnail_7840155d768888.jpg', '<p>sedghjkl;dfghjkl</p>', 'fghjkl;dfgh', 'building contractor', 'project type', '');
 
 --
 -- Indexes for dumped tables
@@ -94,13 +95,11 @@ ALTER TABLE `project`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
